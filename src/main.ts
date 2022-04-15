5737cdd165930c8af9304b608025af0b67590cf0
Phuoc Huynh <huynhphvan@gmail.com>
2022-04-15 23:14:48 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -21,23 +21,27 @@ async function bootstrap() {
       },
     }),
   );
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  });
   const options = new DocumentBuilder()
-  .setTitle('API docs')
-  .setVersion('1.0')
-  .addBearerAuth(
-    {
-      // I was also testing it without prefix 'Bearer ' before the JWT
-      description: `[just text field] Please enter token in following format: Bearer <JWT>`,
-      name: 'Authorization',
-      bearerFormat: 'Bearer', // I`ve tested not to use this field, but the result was the same
-      scheme: 'Bearer',
-      type: 'http', // I`ve attempted type: 'apiKey' too
-      in: 'Header',
-    },
-    'access-token', // This name here is important for matching up with @ApiBearerAuth() in your controller!
-  )
-  .build();
+    .setTitle('API docs')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        // I was also testing it without prefix 'Bearer ' before the JWT
+        description: `[just text field] Please enter token in following format: Bearer <JWT>`,
+        name: 'Authorization',
+        bearerFormat: 'Bearer', // I`ve tested not to use this field, but the result was the same
+        scheme: 'Bearer',
+        type: 'http', // I`ve attempted type: 'apiKey' too
+        in: 'Header',
+      },
+      'access-token', // This name here is important for matching up with @ApiBearerAuth() in your controller!
+    )
+    .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
